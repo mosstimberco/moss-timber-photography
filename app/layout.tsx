71480786d0f368e8b14dc46a3ae9f8a5ecd15e82
Khurrum Husain <khurrum@Mac-2.lan>
2026-09-20 20:30:04 -0700

    Add Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
@@ -23,7 +24,21 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
 className={`${montserrat.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+     <body className="min-h-full flex flex-col">
+  <Script
+    async
+    src="https://connect.facebook.net/en_US/fbevents.js"
+  />
+  <Script
+    dangerouslySetInnerHTML={{
+      __html: `
+        fbq('init', '1551369959634962');
+        fbq('track', 'PageView');
+      `,
+    }}
+  />
+  {children}
+</body>
     </html>
   );
 }
